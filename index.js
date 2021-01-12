ed414c6021c25e55036bda651dd2d0f939611e6e
adam.doskhoev <doskhoyev@gmail.com>
2021-01-12 14:46:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 80
+
+app.get('/', (req, res) => {
+    res.end('<h1>Home Page</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log('Server started')    
+})
